1fe8c282603593e67745c5bee8454009fcef03b0
Thillai <thillaithinesh@gmail.com>
2020-05-03 20:33:42 +0530

    Collections sample code

---
+++ collections/src/com/techbee/collections/ArrayListSample.java
@@ -0,0 +1,17 @@
+package com.techbee.collections;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListSample {
+
+	public static void main(String[] args) {
+		List<String> colors = new ArrayList();
+		colors.add("Green");
+		colors.add("Yellow");
+		colors.add("Red");
+		
+		System.out.println("Colors list : "+colors);
+	}
+}
